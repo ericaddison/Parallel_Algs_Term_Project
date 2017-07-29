651ac0c236d0ab450b18ed1e6f2e7ca8dd25e61f
abruck1 <agbruck@gmail.com>
2017-07-29 15:06:36 -0500

    init commit to new repo (forked from eric)

---
+++ cilk/src/dft.cpp
@@ -0,0 +1,30 @@
+#include "dft.h"
+
+void transform_dft(carray& x, direction dir);
+
+void dft(carray& x)
+{
+  transform_dft(x, FORWARD);
+}
+
+void idft(carray& x)
+{
+  transform_dft(x, REVERSE);
+  for(int i=0; i<x.size(); i++)
+    x[i] /= x.size();
+}
+
+void transform_dft(carray& x, direction dir)
+{
+  const size_t N = x.size();
+  const double v = (2*(dir==REVERSE)-1) * 2 * PI / N;
+
+  // make a copy of x and initialize x to zero
+  carray x_copy(x);
+  x = 0;
+
+  // n^2 dft loop
+  for(int k=0; k<N; k++)
+    for(int j=0; j<N; j++)
+      x[k] += std::polar(1.0, v * j * k) * x_copy[j];
+}
---
+++ cilk/src/ft_helpers.h
@@ -0,0 +1,22 @@
+#ifndef _FT_HELPERS_H
+#define _FT_HELPERS_H
+
+#include <complex>
+#include <cmath>
+#include <valarray>
+#include <iostream>
+
+using std::complex;
+using namespace std::literals::complex_literals;
+typedef std::valarray<complex<double>> carray;
+typedef complex<double> cdouble;
+
+enum direction { FORWARD, REVERSE };
+const double PI = acos(-1);
+
+bool isPow2(int n);
+void checkSize(int n);
+int bitReverse(unsigned int b, int d);
+void bitReverse(carray& x);
+
+#endif
